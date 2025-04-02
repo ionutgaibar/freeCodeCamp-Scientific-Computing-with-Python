189fbec239bdd3948b85478ee999e94b2e1d1f75
Ionut Gaibar <78532008+ionutgaibar@users.noreply.github.com>
2025-04-03 00:38:51 +0300

    Update and rename 4. Learn Python List Comprehension by Building a Case Converter Program.py to 1.4. Learn Python List Comprehension by Building a Case Converter Program.py

--- 1.4. Learn Python List Comprehension by Building a Case Converter Program.py
+++ 1.4. Learn Python List Comprehension by Building a Case Converter Program.py
@@ -1,4 +1,5 @@
 def convert_to_snake_case(pascal_or_camel_cased_string):
+
     snake_cased_char_list = [
         '_' + char.lower() if char.isupper()
         else char
@@ -8,6 +9,8 @@ def convert_to_snake_case(pascal_or_camel_cased_string):
     return ''.join(snake_cased_char_list).strip('_')
 
 def main():
-    print(convert_to_snake_case('aLongAndComplexString'))
+    print(convert_to_snake_case('IAmAPascalCasedString'))
+
+    
 
 main()
